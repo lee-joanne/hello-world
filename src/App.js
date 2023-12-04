de4008080e2568b0daef062bae11e6c8c6c10f42
Joanne Lee <lee.joanne54@gmail.com>
2023-12-04 15:31:42 -0800

    Add hello js example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Greet from './components/Greet';
+import Hello from './components/Hello';
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Hello />
     </div>
   );
 }
 
 export default App;
+
+// next video: 6 - class components
+// . venv/bin/activate
---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hello = () => {
+    return (
+        <div>
+            <h1>Hello Joanne</h1>
+            <p>Blippu</p>
+        </div>
+    )
+}
+
+export default Hello
