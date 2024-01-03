dcd499164d413db286ceed5ec254a3f37819ac31
Joanne Lee <lee.joanne54@gmail.com>
2024-01-02 17:45:57 -0800

    Set up message.js practice file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import Greet from './components/Greet';
 import Hello from './components/Hello';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Greet otherName="Other Name" name="Joanne">
+      {/* <Greet otherName="Other Name" name="Joanne">
         <p>This is children props</p>
       </Greet>
-      <Hello />
+      <Hello /> */}
+      <Message/>
     </div>
   );
 }
 
 export default App;
 
-// next video: 10 - state
+// next video: 11
 // . venv/bin/activate
---
+++ src/components/Message.js
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+
+
+class Message extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            message: 'Welcome Visitor'
+        } // initial state
+    }
+
+    changeMessage () {
+       this.setState({
+            message: "Thank you for subscribing!"
+       }) // state changed after on click event
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>
+                    {this.state.message}
+                </h1>
+                <button onClick={() => this.changeMessage()}>Subscribe</button>
+            </div>
+        )
+    }
+}
+
+export default Message
